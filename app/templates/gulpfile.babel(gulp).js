e8e95bb0ee950e8b2e508b979f05c0e6546e1385
Ændrew Rininsland <aendrew@aendrew.com>
2016-04-21 12:16:26 +0100

    Updates Gulpfile to exclude local.env.js
    The `transpile:server` task was copying it over in TypeScript, which is bad for security and goes against the whole twelve-factor approach to storing credentials in Prod.

--- app/templates/gulpfile.babel(gulp).js
+++ app/templates/gulpfile.babel(gulp).js
@@ -43,7 +43,8 @@ const paths = {
     server: {
         scripts: [
           `${serverPath}/**/!(*.spec|*.integration).js`,
-          `!${serverPath}/config/local.env.sample.js`
+          `!${serverPath}/config/local.env.sample.js`,
+          `!${serverPath}/config/local.env.js`
         ],
         json: [`${serverPath}/**/*.json`],
         test: {
